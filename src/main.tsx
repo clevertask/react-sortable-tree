af596898aad1d5e4792383a3f8ba1b870f261c3d
Roberto Piñón <51218118+gonzalo-novak@users.noreply.github.com>
2024-10-19 14:33:07 -0600

    chore: add a way of getting info of an item by providing its id (#5)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,38 @@
-import { StrictMode, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createOptimizedTreeStructure, SortableTree,} from './index'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { createOptimizedTreeStructure, SortableTree } from "./index";
 
 const App = () => {
-	const [treeStructure, setTreeStructure] = useState(createOptimizedTreeStructure([{ id: "1", label: "Hello", children: [] }, { id: "2", label: "World", children: [] }]))
-	return <SortableTree items={treeStructure.items} onItemsChange={setTreeStructure} isRemovable allowNestedItemAddition showDropIndicator></SortableTree>
-}
+	const [treeStructure, setTreeStructure] = useState(
+		createOptimizedTreeStructure([
+			{ id: "1", label: "Hello", children: [] },
+			{
+				id: "2",
+				label: "World",
+				children: [{ id: "2.2", label: "Hello world!", children: [] }],
+			},
+		])
+	);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
-)
+	return (
+		<SortableTree
+			treeStructure={treeStructure}
+			setTreeStructure={setTreeStructure}
+			isRemovable
+			allowNestedItemAddition
+			showDropIndicator
+		/>
+	);
+};
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+	createRoot(rootElement).render(
+		<StrictMode>
+			<App />
+		</StrictMode>
+	);
+} else {
+	console.error("Root element not found");
+}
