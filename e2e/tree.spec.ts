2e91772835cd1c2b581d63e3a8337eab39af8300
gonzalo-novak <roberto.pinon@proton.me>
2026-01-07 15:01:44 -0600

    chore: add initial tests fro after/before positioning

--- e2e/tree.spec.ts
+++ e2e/tree.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { dragItem } from './drag-item';
 
+test.afterEach(async ({ page }) => {
+  await page.reload();
+});
+
 test('Item A becomes a child of C after drag and drop', async ({ page }) => {
   await page.goto('/');
 
@@ -18,4 +22,58 @@ test('Item A becomes a child of C after drag and drop', async ({ page }) => {
 
   const nestedA = page.getByRole('treeitem', { name: 'A' });
   await expect(nestedA).toHaveAttribute('data-tree-item-parent-id', 'c');
+});
+
+test('Item A is moved below C after drag and drop', async ({ page }) => {
+  await page.goto('/');
+
+  const taskA = page.getByRole('treeitem', { name: 'A' });
+  const taskC = page.getByRole('treeitem', { name: 'C' });
+
+  await expect(taskA).toBeVisible();
+  await expect(taskC).toBeVisible();
+
+  await dragItem({
+    page,
+    from: { name: 'A' },
+    to: { name: 'C', position: 'bottom' },
+  });
+
+  const allItems = page.getByRole('treeitem');
+  const texts = await allItems.allTextContents();
+
+  const indexC = texts.indexOf('C');
+  const indexA = texts.indexOf('A');
+
+  expect(indexA).toBe(indexC + 1);
+
+  // Extra safety: ensure A is NOT nested
+  await expect(taskA).not.toHaveAttribute('data-tree-item-parent-id', 'c');
+});
+
+test('Item C is moved before A after drag and drop', async ({ page }) => {
+  await page.goto('/');
+
+  const taskC = page.getByRole('treeitem', { name: 'C' });
+  const taskA = page.getByRole('treeitem', { name: 'A' });
+
+  await expect(taskC).toBeVisible();
+  await expect(taskA).toBeVisible();
+
+  await dragItem({
+    page,
+    from: { name: 'C' },
+    to: { name: 'A', position: 'top' },
+  });
+
+  const allItems = page.getByRole('treeitem');
+  const texts = await allItems.allTextContents();
+
+  const indexC = texts.indexOf('C');
+  const indexA = texts.indexOf('A');
+
+  expect(indexC).toBe(indexA - 1);
+
+  // Extra safety: ensure C is NOT nested
+  await expect(taskC).not.toHaveAttribute('data-tree-item-parent-id', 'a');
 });
